729f98691af8b74811427bf6efa10880e2083707
Vladislav Gritsenko <vladgritsenko@gmail.com>
2016-02-13 21:24:37 +0200

    added filter

--- renderer/tasks/filter.js
+++ renderer/tasks/filter.js
@@ -1,6 +1,8 @@
 'use strict';
 
-const jimp = require('jimp');
+const jimp  = require('jimp');
+const async = require('async');
+const path  = require('path');
 
 module.exports = function(project) {
     return new Promise((resolve, reject) => {
@@ -13,24 +15,39 @@ module.exports = function(project) {
         // iterate over asset, find images
         for (let asset of project.assets) {
             if (asset.type !== 'image') continue;
+            if (!asset.filters || asset.filters.length < 1) continue;
 
             calls.push((callback) => {
-
+                jimp.read( path.join( project.workpath, asset.name ), (err, image) => {
+                    callback(err, [image, asset]);
+                });
             });
         }
 
-        Jimp.read("lenna.png").then(function (lenna) {
-            lenna.resize(256, 256)            // resize 
-                 .quality(60)                 // set JPEG quality 
-                 .greyscale()                 // set greyscale 
-                 .write("lena-small-bw.jpg"); // save 
-        }).catch(function (err) {
-            console.error(err);
-        });
-
         // run rename(move) callbacks in parallel
         async.parallel(calls, (err, results) => {
-            return (err) ? reject(err) : resolve(project);
-        })
+            if (err) return reject(err);
+
+            // iterate over each vaild asset
+            for (let packed of results) {
+                let image = packed[0];
+                let asset = packed[1];
+
+                // iterate over each defined filter for this asset
+                for (let filter of asset.filters) {
+                    // if that function is defined in jimp library
+                    if (image[filter.name]) {
+                        // call it
+                        image[filter.name].apply(image, filter.params || []);
+                    }
+                }
+
+                // save filtered image
+                image.write( path.join( project.workpath, asset.name ));
+            }
+
+            // pass project to next task
+            resolve(project);
+        });
     });
 };
--- renderer/tasks/rename.js
+++ renderer/tasks/rename.js
@@ -28,6 +28,7 @@ module.exports = function(project) {
 
         // run rename(move) callbacks in parallel
         async.parallel(calls, (err, results) => {
+            console.log(err);
             return (err) ? reject(err) : resolve(project);
         })
     });
