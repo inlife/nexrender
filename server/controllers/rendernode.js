d37ca2a69d0f9c4853e4dbfc0019adc169eed0ea
Vladislav Gritsenko <vladgritsenko@gmail.com>
2016-02-17 00:28:30 +0200

    fixed ticker

--- server/controllers/rendernode.js
+++ server/controllers/rendernode.js
@@ -2,26 +2,33 @@
 
 // rendeing nodes (keepAlive) [in memory array, autodelete on timer]
 let rendernodes = {};
+let running     = false;
 
-// every 1 minute launch clearer for rendernodes
-setInterval(() => {
-    for (let rnodeid in rendernodes) {
-        
-        let rnode = rendernodes[rnodeid];
-        
-        // calculate diffreence
-        let difference = new Date - rnode.updatedAt;
-        
-        // if it bigger then 6 minutes
-        if (difference > 360000) {
-            delete rendernodes[rnodeid];
+let ticker = () => {
+    // every 1 minute launch clearer for rendernodes
+    setInterval(() => {
+        for (let rnodeid in rendernodes) {
+            
+            let rnode = rendernodes[rnodeid];
+            
+            // calculate diffreence
+            let difference = new Date - rnode.updatedAt;
+            
+            // if it bigger then 6 minutes
+            if (difference > 360000) {
+                delete rendernodes[rnodeid];
+            }
         }
-    }
-}, 60000);
+    }, 60000);
+};
 
 module.exports = {
     update: function(req) {
         var data = req.body;
+        if (!running) {
+            running = true;
+            ticker();
+        }
 
         // if does not exists - add to array of existings nodes
         if (!rendernodes.hasOwnProperty( data.uid )) {
