072d070345ce095ce879c5063d5fe332c77b1743
Vladislav Gritsenko <vladgritsenko@gmail.com>
2016-02-17 19:15:54 +0200

    added basic cli interface

---
+++ cli.js
@@ -0,0 +1,40 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const cli       = require('cli').enable('version');
+const noxrender = require('./index.js');
+
+cli.parse({
+    'aerender':     [false, 'PATH to aerender binary', 'path'],
+    'api-server':   ['s', 'Start api server'],
+    'renderer':     ['r', 'Start renderer'],
+    'host':         ['h', 'Remote HOST:PORT to connect', 'host', 'localhost:3000'],
+    'port':         ['p', 'Listen on port', 'port', 3000]
+});
+
+
+// nexrender --renderer --host=localhost:3000 --aerender="/Applications/Adobe After Effects CC 2015/aerender"
+//           -rh localhost:3000 --aerender="/Applications/Adobe After Effects CC 2015/aerender"
+// nexrender --api-server --port=3000
+//           -sp 3000
+
+cli.main(function(args, options) {
+    console.log(args);
+
+    if (options['api-server']) {
+        noxrender.server.start(options.port);
+    }
+
+    if (options['renderer']) {
+        if (!options.aerender) {
+            return console.error('[error] provide --aerender=PATH for aerender binary file');
+        }
+
+        process.env.AE_BINARY = options.aerender;
+
+        let host = options.host.split(':');
+
+        renderer.start( host[0], host[1] );
+    }
+});
