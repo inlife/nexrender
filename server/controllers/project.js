4baecf18b5f1fb41f10a7b9c224906f75d5d80f6
Vladislav Gritsenko <vladgritsenko@gmail.com>
2016-02-20 22:31:10 +0200

    removed console log

--- server/controllers/project.js
+++ server/controllers/project.js
@@ -81,8 +81,6 @@ class Controller {
         // set default data
         data.updatedAt = new Date;
 
-        console.log(data);
-
         // update data and return
         return new Promise((resolve, reject) => {
             resolve( this.db.chain().find({ uid: id }).assign( data ).value() );
