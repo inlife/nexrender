e5e8788afbaea079c9eecb9dba2bf72b008d46f3
Vladislav Gritsenko <vladgritsenko@gmail.com>
2016-02-25 01:34:32 +0200

    removed coverage folder

--- test/index.js
+++ test/index.js
@@ -3,6 +3,14 @@
 // override console.info (disable output from modules)
 console.info = function() {};
 
+const path = require('path');
+
+// override paths for test folder
+process.env.TEMP_DIRECTORY      = path.join('test', 'temp');
+process.env.TEMPLATES_DIRECTORY = path.join('test', 'templates');
+
+const nexrender = require('..');
+
 describe('Testing project', () => {
     require('./renderer/');
     require('./server/');
