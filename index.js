94d0edd514a25e7cd596d45bd65927d3ecf1ec58
Vladislav Gritsenko <vladgritsenko@gmail.com>
2016-02-25 01:38:12 +0200

    removed winston

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
 'use strict';
 
-// setup global logging
-global.winston = require('winston');
-
-// add timestamping
-// winston.remove( winston.transports.Console );
-// winston.add( winston.transports.Console, { 'timestamp': true } );
-
 module.exports = {
     api:        require('./api'),
     server:     require('./server'),
