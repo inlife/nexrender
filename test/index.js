ce907d51e6d566633ee9e12159ebc09c6c95bc1b
Vladislav Gritsenko <vladgritsenko@gmail.com>
2016-02-24 19:16:26 +0200

    added test case for setup task

--- test/index.js
+++ test/index.js
@@ -1,16 +1,8 @@
-// console.log = function() {}
-
+// override console.info (disable output from modules)
 console.info = function() {};
 
-require('./renderer/tasks/setup.js');
-
-var assert = require("assert"); // node.js core module
-
-
-// describe('Array', function(){
-//     describe('#indexOf()', function(){
-//         it('should return -1 when the value is not present', function(){
-//           assert.equal(-1, [1,2,3].indexOf(4)); // 4 is not present in this array so indexOf returns -1
-//         })
-//     })
-// });
+describe('Testing project', () => {
+    require('./renderer/');
+    require('./server/');
+    require('./api/');
+});
