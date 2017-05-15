e82270e50f561f42e311841c46866f722dfa325d
maruware <me@maruware.com>
2017-05-15 21:58:12 +0900

    fix error on rename asset src with query

--- renderer/tasks/rename.js
+++ renderer/tasks/rename.js
@@ -2,6 +2,7 @@
 
 const fs        = require('fs-extra');
 const path      = require('path');
+const url      = require('url');
 const async     = require('async');
 
 /**
@@ -18,7 +19,7 @@ module.exports = function(project) {
 
         // iterate over each file and create rename(move) callback
         for (let asset of project.assets) {
-            let src = path.join( project.workpath, asset.src.substring( asset.src.lastIndexOf('/') + 1 ));
+            let src = path.join( project.workpath, path.basename(url.parse(asset.src).pathname));
             let dst = path.join( project.workpath, asset.name );
 
             if (src === dst) continue;
