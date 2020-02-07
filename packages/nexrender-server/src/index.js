c3a9adb45fb02ba0a87d9897bdbb33a6954a7b44
Vladyslav Hrytsenko <vladgritsenko@gmail.com>
2020-02-08 00:23:43 +0200

    additional cors fixes

--- packages/nexrender-server/src/index.js
+++ packages/nexrender-server/src/index.js
@@ -1,5 +1,8 @@
-const micro = require('micro')
-const cors  = require('micro-cors')()
+const cors   = require('micro-cors')
+const micro  = require('micro')
+const {send} = require('micro')
+
+const withCors = cors({ allowHeaders: ['X-Requested-With','Access-Control-Allow-Origin','X-HTTP-Method-Override','Content-Type','Authorization','Accept','nexrender-secret']})
 
 const { router, withNamespace } = require('microrouter')
 const { get, post, put, del }   = require('microrouter')
@@ -7,17 +10,27 @@ const { withSecret }            = require('./helpers/secret')
 
 const ns = withNamespace('/api/v1')
 
-const handler = secret => withSecret(secret, cors(router(
+const subhandler = router(
     ns(post('/jobs',       require('./routes/jobs-create'))),
     ns(get('/jobs',        require('./routes/jobs-fetch'))),
     ns(get('/jobs/pickup', require('./routes/jobs-pickup'))),
     ns(get('/jobs/:uid',   require('./routes/jobs-fetch'))),
     ns(put('/jobs/:uid',   require('./routes/jobs-update'))),
     ns(del('/jobs/:uid',   require('./routes/jobs-remove')))
-)))
+)
+
+const handler = secret => {
+    return withCors((req, res) => {
+        if (req.method == 'OPTIONS') {
+            return send(res, 200, 'ok');
+        }
+
+        return withSecret(secret, subhandler)(req, res)
+    })
+}
 
 module.exports = {
-    listen: (port = 3000, secret = '') => (
-        micro(handler(secret)).listen(port)
-    )
+    listen: (port = 3000, secret = '') => {
+        return micro(handler(secret)).listen(port)
+    }
 }
