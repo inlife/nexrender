c0f1c8a6cea0663e3d3015c42820a31d81e74646
Vladyslav Hrytsenko <vladgritsenko@gmail.com>
2020-02-07 23:00:30 +0200

    added cors support to the server

--- packages/nexrender-server/src/index.js
+++ packages/nexrender-server/src/index.js
@@ -1,4 +1,5 @@
 const micro = require('micro')
+const cors  = require('micro-cors')()
 
 const { router, withNamespace } = require('microrouter')
 const { get, post, put, del }   = require('microrouter')
@@ -6,14 +7,14 @@ const { withSecret }            = require('./helpers/secret')
 
 const ns = withNamespace('/api/v1')
 
-const handler = secret => withSecret(secret, router(
+const handler = secret => withSecret(secret, cors(router(
     ns(post('/jobs',       require('./routes/jobs-create'))),
     ns(get('/jobs',        require('./routes/jobs-fetch'))),
     ns(get('/jobs/pickup', require('./routes/jobs-pickup'))),
     ns(get('/jobs/:uid',   require('./routes/jobs-fetch'))),
     ns(put('/jobs/:uid',   require('./routes/jobs-update'))),
     ns(del('/jobs/:uid',   require('./routes/jobs-remove')))
-))
+)))
 
 module.exports = {
     listen: (port = 3000, secret = '') => (
