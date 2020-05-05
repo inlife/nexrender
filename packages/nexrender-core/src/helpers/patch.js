737042b376c75992899a6aa013844c49d2a0f113
Vladyslav Hrytsenko <vladgritsenko@gmail.com>
2020-05-05 08:50:12 +0300

    added patch version detection, and updating

--- packages/nexrender-core/src/helpers/patch.js
+++ packages/nexrender-core/src/helpers/patch.js
@@ -22,8 +22,35 @@ module.exports = (settings) => {
 
     /* check if file has been patched already */
     /* by looking for 'nexrender' in the text anywhere */
-    if (data.indexOf('nexrender') !== -1) {
+    if (data.indexOf('nexrender-patch') !== -1) {
         settings.logger.log('command line patch already is in place')
+
+        const patchedMatch = patched.match(/nexrender-patch-v([0-9\.]+)/)
+        const existingMatch = data.match(/nexrender-patch-v([0-9\.]+)/)
+
+        if (patchedMatch[1] !== existingMatch[1]) {
+            try {
+                settings.logger.log('out-of-date version of the commandLineRenderer.jsx patch is detected, attepmting to update')
+                writeTo(patched, originalFile)
+            } catch (err) {
+                if (err.code == 'EPERM') {
+                    settings.logger.log('\n\n              -- E R R O R --\n');
+                    settings.logger.log('you need to run application with admin priviledges once');
+                    settings.logger.log('to install Adobe After Effects commandLineRenderer.jsx patch\n');
+
+                    if (process.platform == 'win32') {
+                        settings.logger.log('reading/writing inside Program Files folder on windows is blocked')
+                        settings.logger.log('please run nexrender with Administrator Privilidges only ONE TIME, to update the patch\n\n')
+                    } else {
+                        settings.logger.log('you might need to try to run nexrender with "sudo" only ONE TIME to update the patch\n\n')
+                    }
+
+                    process.exit(2);
+                } else {
+                    throw err
+                }
+            }
+        }
 
         if (settings.forceCommandLinePatch) {
             settings.logger.log('forced rewrite of command line patch')
