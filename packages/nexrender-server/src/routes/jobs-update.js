7eddf1ff5b12b0cf4640da4aecefa9d5c9a4e78c
FinnFrotscher <finn.frotscher@googlemail.com>
2020-06-06 22:11:08 +0200

    Update jobs-update.js

--- packages/nexrender-server/src/routes/jobs-update.js
+++ packages/nexrender-server/src/routes/jobs-update.js
@@ -4,7 +4,7 @@ const { validate }      = require('@nexrender/types/job')
 const { update, fetch } = require('../helpers/database')
 
 module.exports = async (req, res) => {
-    const data = await json(req)
+    const data = await json(req, {limit: "100mb"})
     const job  = Object.assign({}, fetch(req.params.uid) || {}, data);
 
     console.log(`updating job ${job.uid}`)
