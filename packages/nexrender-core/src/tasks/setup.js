c747a773db5c93f659423d322cb58f297910421e
pdkn <keanepaddy@gmail.com>
2020-07-13 14:01:42 +0100

    Update setup.js
    Warning spelling error

--- packages/nexrender-core/src/tasks/setup.js
+++ packages/nexrender-core/src/tasks/setup.js
@@ -29,7 +29,7 @@ Since you've choosen a custom outputModule (${job.template.outputModule}),
 nexrender, however, does not know what file extension is expected to be rendered.
 
 To prevent this from happening, please add a string value to "job.template.outputExt".
-An example of how that might look like: { "ouutputModule": "h264", "outputExt": "mp4" }.\n`);
+An example of how that might look like: { "outputModule": "h264", "outputExt": "mp4" }.\n`);
     }
 
     // set default job result file name
