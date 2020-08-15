9685a8caf296e096a6119dc4fde3c43486ea06a8
Morreski <contact@enguerrand.io>
2020-08-15 09:51:53 +0200

    Moved 'getRenderingStatus' function to nexrender-types/job

--- packages/nexrender-server/src/helpers/job-formatter.js
+++
@@ -1,6 +0,0 @@
-
-const getStatusFromJob = job => ({uid: job.uid, state: job.state, type: job.type, renderProgress: job.renderProgress || 0})
-
-module.exports = {
-    getStatusFromJob
-}
--- packages/nexrender-server/src/routes/jobs-status.js
+++ packages/nexrender-server/src/routes/jobs-status.js
@@ -1,13 +1,13 @@
 const { send }  = require('micro')
 const { fetch } = require('../helpers/database')
-const { getStatusFromJob } = require('../helpers/job-formatter')
+const { getRenderingStatus } = require('@nexrender/types/job')
 
 module.exports = async (req, res) => {
     if (req.params.uid) {
         console.log(`fetching job status ${req.params.uid}`)
-        send(res, 200, getStatusFromJob(fetch(req.params.uid)))
+        send(res, 200, getRenderingStatus(fetch(req.params.uid)))
     } else {
         console.log(`fetching status list of all jobs`)
-        send(res, 200, fetch().map(getStatusFromJob))
+        send(res, 200, fetch().map(getRenderingStatus))
     }
 }
