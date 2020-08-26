1818646a948bc0ee92e21116a9ffd04e1afa6683
SaajidJoosab <saajid_joosab@flightcentre.com>
2020-08-26 19:34:47 +1000

    Adding the ability for aciton-encode and action-upload to have their own callbacks

--- packages/nexrender-action-upload/index.js
+++ packages/nexrender-action-upload/index.js
@@ -3,7 +3,10 @@ const path     = require('path')
 const {name}   = require('./package.json')
 const requireg = require('requireg')
 
-module.exports = (job, settings, { input, provider, params }, type) => {
+module.exports = (job, settings, { input, provider, params, ...options }, type) => {
+    let onProgress;
+    let onComplete;
+
     if (type != 'postrender') {
         throw new Error(`Action ${name} can be only run in postrender mode, you provided: ${type}.`)
     }
@@ -18,20 +21,28 @@ module.exports = (job, settings, { input, provider, params }, type) => {
     /* fill absolute/relative paths */
     if (!path.isAbsolute(input)) input = path.join(job.workpath, input);
 
+    if (options.hasOwnProperty('onProgress') && typeof options['onProgress'] == 'function') {
+        onProgress = (job, progress) => options.onProgress(job, progress);
+    }
+
+    if (options.hasOwnProperty('onComplete') && typeof options['onComplete'] == 'function') {
+        onComplete = (job) => options.onComplete(job);
+    }
+
     settings.logger.log(`[${job.uid}] starting action-upload action`)
 
     let requirePackage = ''
     try {
         /* try requiring official providers */
         requirePackage = `@nexrender/provider-${provider}`
-        return requireg('@nexrender/provider-' + provider).upload(job, settings, input, params || {});
+        return requireg(requirePackage).upload(job, settings, input, params || {}, onProgress, onComplete);
 
     } catch (e) {
         if (e.message.indexOf('Could not require module') !== -1) {
             try{
                 /* try requiring custom providers */
                 requirePackage = provider
-                return requireg(provider).upload(job, settings, input, params || {});
+                return requireg(requirePackage).upload(job, settings, input, params || {}, onProgress, onComplete);
 
             } catch(e) {
                 if (e.message.indexOf('Could not require module') !== -1) {
--- packages/nexrender-provider-s3/src/index.js
+++ packages/nexrender-provider-s3/src/index.js
@@ -59,7 +59,7 @@ const download = (job, settings, src, dest, params, type) => {
     })
 }
 
-const upload = (job, settings, src, params) => {
+const upload = (job, settings, src, params, onProgress, onComplete) => {
     const file = fs.createReadStream(src);
 
     if (!params.region) {
@@ -75,12 +75,18 @@ const upload = (job, settings, src, params) => {
         return Promise.reject(new Error('S3 ACL not provided.'))
     }
 
-    const onProgress = (e) => {
+    const onUploadProgress = (e) => {
         const progress = Math.ceil(e.loaded / e.total * 100)
+        if (typeof onProgress == 'function') {
+            onProgress(job, progress);
+        }
         settings.logger.log(`[${job.uid}] action-upload: upload progress ${progress}%...`)
     }
 
-    const onComplete = () => {
+    const onUploadComplete = () => {
+        if (typeof onComplete == 'function') {
+            onComplete(job);
+        }
         settings.logger.log(`[${job.uid}] action-upload: upload complete`)
     }
 
@@ -105,11 +111,11 @@ const upload = (job, settings, src, params) => {
                 }
                 else
                 {
-                    onComplete()
+                    onUploadComplete()
                     resolve()
                 }
             })
-            .on('httpUploadProgress', onProgress)
+            .on('httpUploadProgress', onUploadProgress)
     })
 }
 
