f809525c924eb2b3b38535af56c3eb953f6834e0
Vladyslav Hrytsenko <vladgritsenko@gmail.com>
2021-01-10 09:30:14 +0200

    code: added rimraf, and treat cleanup errors as warnings

--- packages/nexrender-core/src/tasks/cleanup.js
+++ packages/nexrender-core/src/tasks/cleanup.js
@@ -1,36 +1,29 @@
-const fs   = require('fs')
-const path = require('path')
-
-/**
- * Remove directory recursively
- */
-const rmdirRecursively = (target) => {
-    if (!fs.existsSync(target)) return;
-
-    fs.readdirSync(target).map(entry => {
-        const entryPath = path.join(target, entry);
-        const result = fs.lstatSync(entryPath).isDirectory()
-            ? rmdirRecursively(entryPath)
-            : fs.unlinkSync(entryPath)
-    })
-
-    fs.rmdirSync(target)
-}
+const fs     = require('fs')
+const path   = require('path')
+const rimraf = require('rimraf')
 
 /**
  * Clean up all workpath files and remove folder
  */
 module.exports = function(job, settings) {
-    if (!settings.skipCleanup) {
-        settings.logger.log(`[${job.uid}] cleaning up...`);
-        rmdirRecursively(job.workpath)
-
-        settings.logger.log(`[${job.uid}] Temporary AfterEffects project deleted. If you want to inspect it for debugging, use "--skip-cleanup"`)
-    } else {
+    if (settings.skipCleanup) {
         settings.logger.log(`[${job.uid}] skipping the clean up...`);
+        return Promise.resolve(job)
     }
 
-    return Promise.resolve(job)
+    return new Promise((resolve, reject) => {
+        settings.logger.log(`[${job.uid}] cleaning up...`);
+
+        rimraf(job.workpath, {glob: false}, (err) => {
+            if (!err) {
+                settings.logger.log(`[${job.uid}] Temporary AfterEffects project deleted. If you want to inspect it for debugging, use "--skip-cleanup"`)
+            } else {
+                settings.logger.log(`[${job.uid}] Temporary AfterEffects could not be deleted. (Error: ${err.code}). Please delete the folder manually: ${job.workpath}`)
+            }
+
+            resolve(job)
+        })
+    })
 };
 
 module.exports.rmdirr = rmdirRecursively
