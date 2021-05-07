2a96223016d8d03836f83238e5652ef34750ed9e
Vladyslav Hrytsenko <vladgritsenko@gmail.com>
2021-05-07 14:46:48 +0300

    meta: added binary packing for database-redis to server

--- packages/nexrender-server/src/helpers/database.js
+++ packages/nexrender-server/src/helpers/database.js
@@ -1,7 +1,12 @@
-
 const requireg = require('requireg');
 
 const databaseType = process.env.NEXRENDER_DATABASE_PROVIDER;
+
+/* place to register all plugins */
+/* so they will be picked up and resolved by pkg */
+if (process.env.NEXRENDER_REQUIRE_PLUGINS) {
+    require('@nexrender/database-redis');
+}
 
 if (databaseType !== undefined) {
     module.exports = requireg(`@nexrender/database-${databaseType}`);
