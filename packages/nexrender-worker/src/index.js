951f570707c1b75c1f7cf672fb6375d3f4665ba0
Joey Stevens <joey.stevens00@gmail.com>
2022-07-01 21:00:56 -0400

    optionally enable Rollbar with ENABLE_ROLLBAR environment variable and provide access token with ROLLBAR_ACCESS_TOKEN environment variable optionally enable Datadog with ENABLE_DATADOG_APM environment variable

--- packages/nexrender-worker/src/index.js
+++ packages/nexrender-worker/src/index.js
@@ -2,6 +2,25 @@ const { createClient } = require('@nexrender/api')
 const { init, render } = require('@nexrender/core')
 const { getRenderingStatus } = require('@nexrender/types/job')
 
+if(process.env.ENABLE_ROLLBAR) {
+    var Rollbar = require('rollbar');
+    var rollbar = new Rollbar({
+      accessToken: process.env.ROLLBAR_ACCESS_TOKEN,
+      captureUncaught: true,
+      captureUnhandledRejections: true,
+      payload: {
+        code_version: '1.0.0',
+      }
+    });
+}
+
+
+if(process.env.ENABLE_DATADOG_APM) {
+    const tracer = require('dd-trace').init();
+    const renderWithTrace = tracer.wrap('render', render);
+}
+
+
 const NEXRENDER_API_POLLING = process.env.NEXRENDER_API_POLLING || 30 * 1000;
 
 /* TODO: possibly add support for graceful shutdown */
@@ -31,6 +50,63 @@ const nextJob = async (client, settings) => {
     } while (active)
 }
 
+const processJob = async (job) => {
+    try {
+        await client.updateJob(job.uid, job)
+    } catch(err) {
+        console.log(`[${job.uid}] error while updating job state to ${job.state}. Job abandoned.`)
+        console.log(`[${job.uid}] error stack: ${err.stack}`)
+        return;
+    }
+
+    try {
+        job.onRenderProgress = function (job, /* progress */) {
+            try {
+                /* send render progress to our server */
+                client.updateJob(job.uid, getRenderingStatus(job))
+            } catch (err) {
+                if (settings.stopOnError) {
+                    throw err;
+                } else {
+                    console.log(`[${job.uid}] error occurred: ${err.stack}`)
+                }
+            }
+        }
+
+        job = await renderWithTrace(job, settings); {
+            job.state = 'finished';
+            job.finishedAt = new Date()
+        }
+
+        await client.updateJob(job.uid, getRenderingStatus(job))
+    } catch (err) {
+        job.state = 'error';
+        job.error = err;
+        job.errorAt = new Date()
+
+        await client.updateJob(job.uid, getRenderingStatus(job)).catch((err) => {
+            if (settings.stopOnError) {
+                throw err;
+            } else {
+                console.log(`[${job.uid}] error occurred: ${err.stack}`)
+            }
+        });
+
+        if (settings.stopOnError) {
+            throw err;
+        } else {
+            console.log(`[${job.uid}] error occurred: ${err.stack}`)
+        }
+    }
+}
+
+const nextJobSetStarted = async(client, settings) => {
+    await nextJob(client, settings); {
+        job.state = 'started';
+        job.startedAt = new Date()
+    }
+}
+
 /**
  * Starts worker "thread" of continious loop
  * of fetching queued projects and rendering them
@@ -47,58 +123,21 @@ const start = async (host, secret, settings) => {
     const client = createClient({ host, secret });
 
     do {
-        let job = await nextJob(client, settings); {
-            job.state = 'started';
-            job.startedAt = new Date()
+        if(process.env.ENABLE_DATADOG_APM) {
+            const scope = tracer.scope()
+            const jobSpan = tracer.startSpan('job')
+            scope.activate(jobSpan, () => {
+                let job = nextJobSetStarted(client, settings)
+                const jobUidSpan = tracer.startSpan(job.uid)
+                scope.active(jobUidSpan, () => {
+                    processJob(job)
+                })
+            })
         }
-
-        try {
-            await client.updateJob(job.uid, job)
-        } catch(err) {
-            console.log(`[${job.uid}] error while updating job state to ${job.state}. Job abandoned.`)
-            console.log(`[${job.uid}] error stack: ${err.stack}`)
-            continue;
-        }
-
-        try {
-            job.onRenderProgress = function (job, /* progress */) {
-                try {
-                    /* send render progress to our server */
-                    client.updateJob(job.uid, getRenderingStatus(job))
-                } catch (err) {
-                    if (settings.stopOnError) {
-                        throw err;
-                    } else {
-                        console.log(`[${job.uid}] error occurred: ${err.stack}`)
-                    }
-                }
-            }
-
-            job = await render(job, settings); {
-                job.state = 'finished';
-                job.finishedAt = new Date()
-            }
-
-            await client.updateJob(job.uid, getRenderingStatus(job))
-        } catch (err) {
-            job.state = 'error';
-            job.error = err;
-            job.errorAt = new Date()
-
-            await client.updateJob(job.uid, getRenderingStatus(job)).catch((err) => {
-                if (settings.stopOnError) {
-                    throw err;
-                } else {
-                    console.log(`[${job.uid}] error occurred: ${err.stack}`)
-                }
-            });
-
-            if (settings.stopOnError) {
-                throw err;
-            } else {
-                console.log(`[${job.uid}] error occurred: ${err.stack}`)
-            }
-        }
+        else {
+            let job = nextJobSetStarted(client, settings)
+            processJob(job)
+        } 
     } while (active)
 }
 
