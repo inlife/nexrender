2731f490e90de56217e0080becc7ec6634cc629b
Dmitry Bogomya <dmitry.bogomya@gmail.com>
2022-10-14 14:34:55 +0200

    CR-4150 Return automation job history id from getRenderingStatus

--- packages/nexrender-types/job.js
+++ packages/nexrender-types/job.js
@@ -98,6 +98,7 @@ const validate = job => {
 const getRenderingStatus = job => ({
     uid: job.uid,
     jobId: job.jobId || null,
+    jobHistoryId: job.jobHistoryId || null,
     state: job.state,
     type: job.type,
     renderProgress: job.renderProgress || 0,
