4d759436e9ed768f9377f0c234005d3c1af9d158
craigcartmell <craigcartmell1@gmail.com>
2022-10-17 15:33:58 +0100

    CR-4139 - Remove console logging

--- packages/nexrender-server/src/routes/jobs-fetch.js
+++ packages/nexrender-server/src/routes/jobs-fetch.js
@@ -7,8 +7,7 @@ module.exports = async (req, res) => {
 
         send(res, 200, await fetch(req.params.uid))
     } else if(req.query.type) {
-        // TODO: Write test
-        send(res, 200, await fetch(null,req.query.type || 'default'))
+        send(res, 200, await fetch(null,req.query.type))
     } else {
         console.log(`fetching list of all jobs`)
         send(res, 200, await fetch())
