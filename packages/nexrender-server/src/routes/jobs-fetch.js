d19f48f4c22a6f613db8aac90c0770fd545a1cd9
craigcartmell <craigcartmell1@gmail.com>
2022-10-18 15:47:55 +0100

    CR-4139 - Filter jobs by types

--- packages/nexrender-server/src/routes/jobs-fetch.js
+++ packages/nexrender-server/src/routes/jobs-fetch.js
@@ -6,8 +6,11 @@ module.exports = async (req, res) => {
         console.log(`fetching job ${req.params.uid}`)
 
         send(res, 200, await fetch(req.params.uid))
-    } else if(req.query.type) {
-        send(res, 200, await fetch(null,req.query.type))
+    } else if(req.query.types) {
+        const parsedTypes = req.query.types.split(',')
+        console.log(`fetching jobs by types ${parsedTypes.join(',')}`)
+
+        send(res, 200, await fetch(null,parsedTypes))
     } else {
         console.log(`fetching list of all jobs`)
         send(res, 200, await fetch())
