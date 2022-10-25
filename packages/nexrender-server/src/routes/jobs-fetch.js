6ad61923cb465a7ab506c35611d8b58970f7e28b
warrenday <warrenjday@gmail.com>
2022-10-25 09:49:38 +0100

    paginate fetch

--- packages/nexrender-server/src/routes/jobs-fetch.js
+++ packages/nexrender-server/src/routes/jobs-fetch.js
@@ -1,7 +1,29 @@
 const { send }  = require('micro')
 const { fetch } = require('../helpers/database')
 
+const paginate = (results, { page, size }) => {
+    const index = (page - 1) * size;
+    const pagedResults = results
+        .reverse()
+        .slice(index, index + size)
+        .reverse();
+
+    return pagedResults
+}
+
+const clamp = (input, { min, max }) => {
+    if (input < min) {
+        return min;
+    }
+    if (max && input > max) {
+        return max;
+    }
+    return input;
+};
+
 module.exports = async (req, res) => {
+    const { page, size = 20 } = req.query || {};
+
     if (req.params.uid) {
         console.log(`fetching job ${req.params.uid}`)
 
@@ -13,6 +35,13 @@ module.exports = async (req, res) => {
         send(res, 200, await fetch(null,parsedTypes))
     } else {
         console.log(`fetching list of all jobs`)
-        send(res, 200, await fetch())
+        const results = await fetch()
+        const finalResults = page ?
+            paginate(results, {
+                page: clamp(page, { min: 1 }),
+                size: clamp(size, { min: 1, max: 30 })
+            })
+            : results
+        send(res, 200, finalResults)
     }
 }
