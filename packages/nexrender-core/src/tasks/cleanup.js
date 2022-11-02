f35249485c70ac639585e0fbf67ece6e9208d89a
gffuma <fumagalli.gf@gmail.com>
2022-11-02 21:56:30 +0100

    Use path join in cleanup

--- packages/nexrender-core/src/tasks/cleanup.js
+++ packages/nexrender-core/src/tasks/cleanup.js
@@ -1,4 +1,5 @@
 const rimraf = require('rimraf')
+const path = require('path')
 
 /**
  * Clean up all workpath files and remove folder
@@ -11,12 +12,12 @@ module.exports = function(job, settings) {
 
     return new Promise((resolve) => {
         settings.logger.log(`[${job.uid}] cleaning up...`);
-        
+
         // sometimes this attribute (workpath) is undefined
         if (!job.workpath) {
-            job.workpath = settings.workpath.concat('/', job.uid, '/')
+            job.workpath = path.join(settings.workpath, job.uid)
         }
-       	
+
         rimraf(job.workpath, {glob: false}, (err) => {
             if (!err) {
                 settings.logger.log(`[${job.uid}] Temporary AfterEffects project deleted. If you want to inspect it for debugging, use "--skip-cleanup"`)
