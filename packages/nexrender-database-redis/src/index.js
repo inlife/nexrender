2278d0f81a3790459f74e649b0da64fc6abc5dab
craigcartmell <craigcartmell1@gmail.com>
2022-11-03 10:02:16 +0000

    CR-4193 - Fix filterAndSortJobs import

--- packages/nexrender-database-redis/src/index.js
+++ packages/nexrender-database-redis/src/index.js
@@ -55,7 +55,7 @@ const fetch = async (uid, types = []) => {
             return JSON.parse(value)
         })
 
-        return filterAndSort(results, types)
+        return filterAndSortJobs(results, types)
     }
 }
 
