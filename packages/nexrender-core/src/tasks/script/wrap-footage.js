ee838df0ac7a5e5e9b35246ab101f8c0942378d5
Mikael <mikael@madboxgames.io>
2024-07-31 10:17:29 +0200

    add apostrophe escapement to wrapFootage

--- packages/nexrender-core/src/tasks/script/wrap-footage.js
+++ packages/nexrender-core/src/tasks/script/wrap-footage.js
@@ -6,7 +6,7 @@ const wb = (value) => (value ? 'true' : 'false')
 
 const wrapFootage = (job, settings, { dest, ...asset }) => (`(function() {
     ${selectLayers(job, asset, `function(layer) {
-        nexrender.replaceFootage(layer, '${checkForWSL(dest.replace(/\\/g, "\\\\"), settings)}', ${wb(asset.sequence)}, ${wb(asset.removeOld)})
+        nexrender.replaceFootage(layer, '${checkForWSL(dest.replace(/\\/g, "\\\\").replace(/'/,"\\'"), settings)}', ${wb(asset.sequence)}, ${wb(asset.removeOld)})
     }`)}
 })();\n`)
 
